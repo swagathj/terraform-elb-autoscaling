856f0a509caeff48b21c15ca285b18cc6bb3861c
Swagath <jswagath@gmail.com>
2019-09-22 11:32:22 +0530

    Modified ec2

--- ec2-module.tf
+++ ec2-module.tf
@@ -1,10 +1,10 @@
 module "ec2-app-v1" {
   source = "./modules/ec2"
   region = "ap-south-1"
-  key-name = "power"
+  key-name = "test"
   ami-id = "ami-0cb0e70f44e1a4bb5"
   instance-type = "t2.micro"
-  number-of-ec2-instances-required = "3"
+  number-of-ec2-instances-required = "2"
   public-key-file-name = "${file("./modules/ec2/power.pub")}"
 
   instance-name-taq = "cloudelligent-vpn"
@@ -12,11 +12,8 @@ module "ec2-app-v1" {
 
   vpc-security-group-ids = "${module.cloudelligent-ec2-sg.ec2-sg-security-group}"
   ec2-subnets-ids = ["${module.vpc.public-subnet-ids}"]
-
   #IN CASE OF LAUNCHING EC2 IN SPECIFIC SUBNETS OR PRIVATE SUBNETS, PLEASE UN-COMMENT BELOW"
  # ec2-subnets-ids = ["${module.cloudelligent-vpc.private-subnet-ids}"]
  # ec2-subnets-ids = ["","","","","",""]
  # user-data = "${file("./modules/ec2/httpd.sh")}"
-
-
 }
